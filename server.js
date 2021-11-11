e6a1010123035889b4d903442d84c3d23b7422aa
Inna <inna.kachinskaya13@gmail.com>
2021-11-12 00:35:57 +0200

    Add server with comments

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
+//ЛОГИКА ДЛЯ СОЗДАНИЯ СЕРВЕРА
+
+//импортируем из ноды модуль для создания сервера
 const http = require("http");
-const app=require('./app');
+
+//импортируем приложение
+const app = require("./app");
+
+//создаем сервер и запускаем приложение - экспресс
 const server = http.createServer(app);
 
+//создаем порт, берем из переменной окружения || 3000
 const PORT = process.env.PORT || 3000;
 
+//запускаем сервер, функция отрабабтывает, когда сервер запускается
 server.listen(PORT, () => {
   console.log(`Server is active on ${PORT}`);
 });
