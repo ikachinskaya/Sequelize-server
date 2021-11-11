d149f5173f9da33bdfa40a5343f67c9bf3e095c9
Inna <inna.kachinskaya13@gmail.com>
2021-11-12 00:41:18 +0200

    Add User CRUD

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
+//ЛОГИКА ДЛЯ ЭКСПРЕССА
+//ЗАДАЧА: получать запросы от клиента и кидать их контроллеру
+//импортируем экспресс
 const express = require("express");
-const UserController = require("./controllers/userControllers");
+
+//создаем экспресс-приложение
 const app = express();
 
-//для работы с JSON файлами
-const bodyParser = express.json(); //data stream->JSON->JS объект->req.body
-app.use(bodyParser);
+//импортируем UserController
+const UserController = require("./controllers/userControllers");
 
+//специальная промежуточная функция(middleWare), которая знает, как обрабатывать полученные JSON-данные
+const bodyParser = express.json(); //data stream->JSON->JS объект->req.body
+
+//подключаем. use передаст эту функцию всем методам
+app.use(bodyParser);
+//теперь сервер умеет работать с JSON-файлами
+
+//запрос на получение данных
+//говорим каким методом, передаем путь и функцию, которую нужно исполнить
 app.get("/users", UserController.getUsers);
 
-app.post("/user");
+//запрос на создание данных
+app.post("/user", UserController.createUser);
+
+app.patch("/user/:id", UserController.updateUser);
+
+app.post("/user/:id", UserController.getUser);
+
+app.delete("/user/:id", UserController.deleteUser);
+
+//экспортируем приложение
 module.exports = app;
--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,3 +1,98 @@
+const { User } = require("../models");
+
+//вернуть определенные аттрибуты
+// module.exports.getUsers = async (req, res, next) => {
+//   try {
+//     const users = await User.findAll({
+//       attributes: ["firstName", "lastName", "email"],
+//     });
+//     res.send(users);
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+//===================================================================
+//вернуть всё, кроме пароля
 module.exports.getUsers = async (req, res, next) => {
-  res.send("Юзеров нет");
+  try {
+    const users = await User.findAll({
+      attributes: {
+        exclude: ["password"],
+      },
+    });
+    res.send(users);
+  } catch (error) {
+    next(error);
+  }
 };
+//===================================================================
+//вернуть одного пользователя
+module.exports.getUser = async (req, res, next) => {
+  try {
+    const {
+      body,
+      params: { id },
+    } = req;
+    const user = await User.findAll({
+      where: { id },
+    });
+    res.send(user);
+  } catch (error) {
+    next(error);
+  }
+};
+//===================================================================
+module.exports.updateUser = async (req, res, next) => {
+  try {
+    const {
+      body,
+      params: { id },
+    } = req;
+    const [updatedRows, [updatedUser]] = await User.update(body, {
+      where: { id },
+      returning: true,
+      //returning: ["first_name" , "last_name"]
+    });
+
+    const temp = updatedUser.get();
+    delete temp.password;
+
+    res.send(updatedUser);
+  } catch (error) {
+    next(error);
+  }
+};
+//===================================================================
+module.exports.deleteUser = async (req, res, next) => {
+  try {
+    const {
+      body,
+      params: { id },
+    } = req;
+
+    // const foundUser = await User.findAll({
+    //   where: { id },
+    // });
+
+    const foundUser = await User.findByPk(id);
+    await foundUser.destroy();
+
+    res.send(foundUser);
+  } catch (error) {
+    next(error);
+  }
+};
+//===================================================================
+module.exports.createUser = async (req, res, next) => {
+  try {
+    //body- JSON с юзером
+    const { body } = req;
+    const newUser = await User.create(body);
+    res.send(newUser);
+  } catch (error) {
+    next(error);
+  }
+};
+
+//===================================================================
+//findByPk - ищет по первичному ключу
